56379de954ecb39ea7b594335ecbd72f65c68ab9
LISIJING <823205964@qq.com>
2022-07-04 16:58:13 +0800

    feat: 02

--- statement.js
+++ statement.js
@@ -1,35 +1,74 @@
 function statement(invoice, plays) {
-	let totalAmount = 0;
-	let volumeCredits = 0;
 	let result = `Statement for ${invoice.customer}\n`;
+	result += getPerformanceDetails(invoice, plays);
+	result += `Amount owed is ${formatUSD(getTotalAmount(invoice, plays))}\n`;
+	result += `You earned ${getTotalCredits(invoice, plays)} credits\n`;
+	return result;
+}
+
+module.exports = statement;
+
+function getPerformanceDetails(invoice, plays) {
+	let details = '';
 	for (let perf of invoice.performances) {
 		const play = plays[perf.playID];
 		let thisAmount = 0;
 		switch (play.type) {
 			case 'tragedy':
 				thisAmount = geTragedyAmount(perf);
-				volumeCredits += getTragedyVolumeCredits(perf);
 				break;
 			case 'comedy':
 				thisAmount = getComedyAmount(perf);
-				volumeCredits += getComedyVolumeCredits(perf);
 				break;
 			default:
 				throw new Error(`unknown type: ${play.type}`);
 		}
 
 		// print line for this order
-		result += `  ${play.name}: ${formatUSD(thisAmount)} (${
+		details += `  ${play.name}: ${formatUSD(thisAmount)} (${
 			perf.audience
 		} seats)\n`;
-		totalAmount += thisAmount;
 	}
-	result += `Amount owed is ${formatUSD(totalAmount)}\n`;
-	result += `You earned ${volumeCredits} credits\n`;
-	return result;
+	return details;
 }
 
-module.exports = statement;
+function getTotalAmount(invoice, plays) {
+	let totalAmount = 0;
+	for (let perf of invoice.performances) {
+		const play = plays[perf.playID];
+		let thisAmount = 0;
+		switch (play.type) {
+			case 'tragedy':
+				thisAmount = geTragedyAmount(perf);
+				break;
+			case 'comedy':
+				thisAmount = getComedyAmount(perf);
+				break;
+			default:
+				throw new Error(`unknown type: ${play.type}`);
+		}
+		totalAmount += thisAmount;
+	}
+	return totalAmount;
+}
+
+function getTotalCredits(invoice, plays) {
+	let volumeCredits = 0;
+	for (let perf of invoice.performances) {
+		const play = plays[perf.playID];
+		switch (play.type) {
+			case 'tragedy':
+				volumeCredits += getTragedyVolumeCredits(perf);
+				break;
+			case 'comedy':
+				volumeCredits += getComedyVolumeCredits(perf);
+				break;
+			default:
+				throw new Error(`unknown type: ${play.type}`);
+		}
+	}
+	return volumeCredits;
+}
 
 function formatUSD(number) {
 	return new Intl.NumberFormat('en-US', {
